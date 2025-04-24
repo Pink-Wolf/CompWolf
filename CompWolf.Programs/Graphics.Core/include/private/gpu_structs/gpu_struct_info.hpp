c7707ace03c3ee065a3642877bfd0e49bcc92342
Pink-Wolf <apple6and6tomato@gmail.com>
2025-04-24 08:21:52 +0200

    Graphics.Docs: Fixed wrong javadoc comment

--- CompWolf.Programs/Graphics.Core/include/private/gpu_structs/gpu_struct_info.hpp
+++ CompWolf.Programs/Graphics.Core/include/private/gpu_structs/gpu_struct_info.hpp
@@ -10,7 +10,7 @@ namespace compwolf
 	/** A type trait class for types passed to the GPU.
 	 * That is, to be able to pass your data type to the GPU, you must specialize this template with your type, so as to fulfill [[ShaderField]].
 	 * You can use [[new_gpu_struct_info]] to easily create your specialization.
-	 * @typedata DataType The type to get info about, for the GPU.
+	 * @typeparam DataType The type to get info about, for the GPU.
 	 */
 	template <typename DataType>
 	struct gpu_struct_info
