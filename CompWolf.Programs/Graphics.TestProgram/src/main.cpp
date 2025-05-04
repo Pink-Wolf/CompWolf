3e44d0e2cd501c90e2a32a61dba750498d056ce7
Pink-Wolf <apple6and6tomato@gmail.com>
2025-05-04 17:38:29 +0200

    Graphics.TestProgram: cleaned up main

--- CompWolf.Programs/Graphics.TestProgram/src/main.cpp
+++ CompWolf.Programs/Graphics.TestProgram/src/main.cpp
@@ -39,31 +39,38 @@ int main()
 		, {.75f, .125f, .5f}
 	);
 
+
 	auto clock = std::chrono::high_resolution_clock();
 	auto start_time = clock.now();
-	int frame_count = 0;
-	int frames_per_report = 60;
+	double elapsed_time = 0.;
+	double delta_time = 0.;
+
+	int frames_since_last_report = 0;
+	double next_report_time = 1.;
 
 	while (window.running())
 	{
 		window.update_image();
 		environment.update();
 
-		if (environment.inputs().state_for('w').down()) square.transform().data()[0].position.y() -= .0005f;
-		if (environment.inputs().state_for('a').down()) square.transform().data()[0].position.x() -= .0005f;
-		if (environment.inputs().state_for('s').down()) square.transform().data()[0].position.y() += .0005f;
-		if (environment.inputs().state_for('d').down()) square.transform().data()[0].position.x() += .0005f;
+		if (environment.inputs().state_for('w').down()) square.transform().data()[0].position.y() -= delta_time;
+		if (environment.inputs().state_for('a').down()) square.transform().data()[0].position.x() -= delta_time;
+		if (environment.inputs().state_for('s').down()) square.transform().data()[0].position.y() += delta_time;
+		if (environment.inputs().state_for('d').down()) square.transform().data()[0].position.x() += delta_time;
 
-		++frame_count;
-		if (frame_count >= frames_per_report) [[unlikely]]
+		// get new time
+		auto old_time = elapsed_time;
+		elapsed_time = std::chrono::duration<double>(clock.now() - start_time).count();
+		delta_time = elapsed_time - old_time;
+
+		// report framerate
+		++frames_since_last_report;
+		if (elapsed_time >= next_report_time) [[unlikely]]
 		{
-			auto elapsed_time = std::chrono::duration<double>(clock.now() - start_time).count();
-			auto framerate = frame_count / elapsed_time;
+			auto framerate = frames_since_last_report / (elapsed_time - next_report_time + 1);
 			std::cout << "framerate: " << framerate << std::endl;
-
-			frames_per_report = std::ceil(framerate);
-			frame_count = 0;
-			start_time = clock.now();
+			next_report_time = elapsed_time + 1;
+			frames_since_last_report = 0;
 		}
 	}
 
